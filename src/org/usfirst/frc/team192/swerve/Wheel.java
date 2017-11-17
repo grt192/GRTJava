d4038e9ce5d690314967d948e07da1ad072f0aeb
Jack Jefferson <eptgen@gmail.com>
2017-11-16 16:08:20 -0800

    added swerve things

---
+++ src/org/usfirst/frc/team192/swerve/Wheel.java
@@ -0,0 +1,74 @@
+package org.usfirst.frc.team192.swerve;
+
+
+public class Wheel
+{
+	private double robotWidth;
+	private double robotHeight;
+	private double deltaTheta;
+	private int positiveX;
+	private int positiveY;
+	
+	public Wheel(double robotWidth, double robotHeight, int positiveX, int positiveY)
+	{
+		this.robotWidth = robotWidth;
+		this.robotHeight = robotHeight;
+		this.deltaTheta = Math.atan(robotHeight / robotWidth);
+		this.positiveX = positiveX; // 1 or -1
+		this.positiveY = positiveY; // 1 or -1
+	}
+	
+	public double realAtan(double x, double y)
+	{
+		double first = Math.atan(y / x);
+		// int xNorm = (int) (x / Math.abs(x));
+		int yNorm = (int) (y / Math.abs(y));
+		if (x >= 0)
+		{
+			return first + Math.PI + (-1 * Math.PI) * yNorm;
+		}
+		else
+		{
+			return first + Math.PI;
+		}
+	}
+	
+	public WheelVelocity update(double robotTheta, double rv, double vx, double vy)
+	{
+		double wheelCurrentTheta = robotTheta + deltaTheta;
+		double r = Math.sqrt(robotWidth * robotWidth / 4 + robotHeight * robotHeight / 4);
+		double dx = (r * Math.cos(wheelCurrentTheta)) * positiveX;
+		double dy = (r * Math.sin(wheelCurrentTheta)) * positiveY;
+		double actualvx = vx + rv * dy;
+		double actualvy = vy - rv * dx;
+		double wheelTheta = realAtan(actualvx, actualvy);
+		double speed = Math.sqrt(actualvx * actualvx + actualvy * actualvy);
+		return new WheelVelocity(speed, wheelTheta);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/org/usfirst/frc/team192/swerve/WheelVelocity.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team192.swerve;
+
+public class WheelVelocity 
+{
+	private double speed;
+	private double angle;
+	
+	public WheelVelocity(double speed, double angle)
+	{
+		this.speed = speed;
+		this.angle = angle;
+	}
+}
