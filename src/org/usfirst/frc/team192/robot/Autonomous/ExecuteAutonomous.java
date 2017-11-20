e055cdc7773851300b495ef59f23066cdb162af1
Chela <chela@Chelas-MacBook-Air-2.local>
2017-11-20 12:22:03 -0800

    added commands to autonomous and joystick inputs to teleop

---
+++ src/org/usfirst/frc/team192/robot/Autonomous/ExecuteAutonomous.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team192.robot.Autonomous;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+public class ExecuteAutonomous extends IterativeRobot {
+
+    Command autonomousCommand;
+
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+        // instantiate the command used for the autonomous period
+        //autonomousCommand = new *insert name of mechanism here*();
+    }
+
+    public void autonomousInit() {
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null) autonomousCommand.start();
+    }
+
+    //This function is called periodically during autonomous
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+    
+}
