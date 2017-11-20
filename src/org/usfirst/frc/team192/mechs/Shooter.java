da89b31429b6e321f9351ed824b15ba7c0452fa7
coolpeepz <colin.r.schultz@gmail.com>
2017-11-20 14:43:22 -0800

    Added shooter mech skeleton code

---
+++ src/org/usfirst/frc/team192/mechs/Shooter.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team192.mechs;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Shooter {
+
+	public Shooter(XboxController controller, CANTalon flywheelMotor, CANTalon turntableMotor) {
+		// Will also need something else to push the balls into the flywheel
+	}
+
+	public void shoot() {
+
+	}
+
+	public void setFlywheelSpeed(double speed) {
+
+	}
+
+	public void rotateByAngle(double theta) {
+
+	}
+
+	public void setTurnTableSpeed(double speed) {
+
+	}
+
+	public void rotateByController() {
+
+	}
+
+}
