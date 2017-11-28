0814643cb3ee3755b9a1848c27553553a0c30190
Jack Jefferson <eptgen@gmail.com>
2017-11-28 14:56:12 -0800

    added config stuff

---
+++ src/org/usfirst/frc/team192/config/Config.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team192.config;
+
+public class Config
+{
+	public static double ROBOT_WIDTH = 3.1415926; // placeholder (meters)
+	public static double ROBOT_HEIGHT = 3.1415926; // placeholder (meters)
+}
