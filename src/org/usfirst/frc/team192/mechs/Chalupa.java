c56f35c1fcbd472948ccf067aa11d3fbbffaad3c
coolpeepz <colin.r.schultz@gmail.com>
2017-11-27 16:50:41 -0800

    Added chalupa and lever mechs

---
+++ src/org/usfirst/frc/team192/mechs/Chalupa.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team192.mechs;
+
+import com.ctre.CANTalon;
+
+public class Chalupa {
+
+	public Chalupa(CANTalon motor) {
+
+	}
+
+	public void set(double speed) {
+
+	}
+
+	public void set(boolean on) {
+
+	}
+
+	public void toggle() {
+
+	}
+
+}
---
+++ src/org/usfirst/frc/team192/mechs/Lever.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team192.mechs;
+
+public class Lever {
+
+	public Lever() {
+		// I don't know how this mech works
+	}
+
+	public void up() {
+
+	}
+
+	public void down() {
+
+	}
+
+	public void downAndUp() {
+
+	}
+}
