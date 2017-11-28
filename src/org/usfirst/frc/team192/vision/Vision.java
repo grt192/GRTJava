6bf50a3738b838513da0a1051124a8d360b581ad
coolpeepz <colin.r.schultz@gmail.com>
2017-11-27 16:17:01 -0800

    Fixed compile errors in Vision.java

--- src/org/usfirst/frc/team192/vision/Vision.java
+++ src/org/usfirst/frc/team192/vision/Vision.java
@@ -1,69 +1,35 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
+package org.usfirst.frc.team192.vision;
 
-public class Vision
-{
-	//int[][] matrix = new int[100][200]; 
-	Mat img = Imgcodecs.imread("path here"); 
-	
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+public class Vision {
+	// int[][] matrix = new int[100][200];
+	Mat img = Imgcodecs.imread("path here");
+
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public static void readImageAndConvert() {
-		//read in image and store as matrix of pixel values
+		// read in image and store as matrix of pixel values
 	}
-	
+
 	public static void getBoundaries(int[][] matrix) {
-		//boundary detection for tape
+		// boundary detection for tape
 	}
-	
+
 	public static Mat outputData() {
-		//return matrix of 0s for non-tape, 1s for tape
-	}	
+		return null;
+		// return matrix of 0s for non-tape, 1s for tape
+	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-
-public class Hello
-{
-   public static void main( String[] args )
-   {
-      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-      Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-      System.out.println( "mat = " + mat.dump() );
-   }
-}*/
+/*
+ * import org.opencv.core.Core; import org.opencv.core.CvType; import
+ * org.opencv.core.Mat;
+ * 
+ * public class Hello { public static void main( String[] args ) {
+ * System.loadLibrary( Core.NATIVE_LIBRARY_NAME ); Mat mat = Mat.eye( 3, 3,
+ * CvType.CV_8UC1 ); System.out.println( "mat = " + mat.dump() ); } }
+ */
