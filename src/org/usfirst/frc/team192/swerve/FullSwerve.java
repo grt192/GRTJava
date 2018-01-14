b5786dab4da3bdb73e12cb14fc4740179a62c307
coolpeepz <colin.r.schultz@gmail.com>
2018-01-13 16:45:54 -0800

    Added a bunch of comments to express my confusion

--- src/org/usfirst/frc/team192/swerve/FullSwerve.java
+++ src/org/usfirst/frc/team192/swerve/FullSwerve.java
@@ -2,13 +2,13 @@ package org.usfirst.frc.team192.swerve;
 
 import org.usfirst.frc.team192.robot.JoystickInput;
 
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class FullSwerve extends SwerveBase {
 	private ADXRS450_Gyro gyro;
-	
+
 	private final double DRIVE_RATIO = 1.0 / 2;
 
 	public FullSwerve(double robotWidth, double robotHeight, ADXRS450_Gyro gyro) {
@@ -32,22 +32,26 @@ public class FullSwerve extends SwerveBase {
 		double currentAngle = Math.toRadians(gyro.getAngle());
 		for (int i = 0; i < 4; i++) {
 			double r = Math.sqrt(robotWidth * robotWidth + robotHeight * robotHeight) / 2;
-			double wheelAngle = Math.atan2(robotWidth, -robotHeight);
+			double wheelAngle = Math.atan2(robotWidth, -robotHeight); // should be (robotWidth, robotHeight) ?? because
+																		// x axis and y axis are swapped but not
+																		// reversed
 			if (i == 0 || i == 3) {
 				wheelAngle *= -1;
 			}
 			if (i == 2 || i == 3) {
 				wheelAngle += Math.PI;
 			}
-			wheelAngle -= currentAngle;
-			double dx = r * Math.cos(Math.PI / 2 - wheelAngle);
-			double dy = r * Math.sin(Math.PI / 2 - wheelAngle);
-			double actualvx = vx + rv * dy;
+			wheelAngle -= currentAngle; // I think you need to add the gyro angle
+			double dx = r * Math.cos(Math.PI / 2 - wheelAngle); // I think it just has to be cos(wheelAngle) because the
+																// axes are already swapped
+			double dy = r * Math.sin(Math.PI / 2 - wheelAngle); // see above
+			double actualvx = vx + rv * dy; // probably multiply rv by a scalar so it has less influence
 			double actualvy = vy - rv * dx;
-			double wheelTheta = realAtan(actualvx, actualvy); // haha
-			double speed = Math.sqrt(actualvx * actualvx + actualvy * actualvy);
-			wheels[i].setDriveSpeed(speed * DRIVE_RATIO);
-			wheels[i].setTargetPosition(wheelTheta);
+			double wheelTheta = realAtan(actualvx, actualvy); // realAtan() also inverts y. I don't think this is
+																// necessary
+			double speed = Math.sqrt(actualvx * actualvx + actualvy * actualvy); // at least this part makes sense :)
+			wheels[i].setDriveSpeed(speed * DRIVE_RATIO); // we also need to normalize it do it is never >1
+			wheels[i].setTargetPosition(wheelTheta); // don't you need to subtract the gyro angle?
 		}
 	}
 
