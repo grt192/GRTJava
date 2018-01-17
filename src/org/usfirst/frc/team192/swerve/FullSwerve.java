e624dc83e979938a5f6e84ebfd20938df6798f8d
Jack Jefferson <eptgen@gmail.com>
2018-01-16 16:22:07 -0800

    wheels move conditionally when speed > 0.1 on a case-by-case basis

--- src/org/usfirst/frc/team192/swerve/FullSwerve.java
+++ src/org/usfirst/frc/team192/swerve/FullSwerve.java
@@ -11,7 +11,7 @@ public class FullSwerve extends SwerveBase {
 	private ADXRS450_Gyro gyro;
 	private final double MAX_JOYSTICK_VALUE = Math.sqrt(2);
 	private final double MAX_ROTATE_VALUE = 1;
-	private final double ROTATE_SCALE;
+	private double ROTATE_SCALE;
 
 	public FullSwerve(double robotWidth, double robotHeight, ADXRS450_Gyro gyro) {
 		super(robotWidth, robotHeight);
@@ -42,42 +42,39 @@ public class FullSwerve extends SwerveBase {
 	protected void changeMotors(double rv, double vx, double vy) {
 		double currentAngle = Math.toRadians(gyro.getAngle());
 		rv *= ROTATE_SCALE * -1;
+		vx *= SPEED_SCALE;
+		vy *= SPEED_SCALE;
 		SmartDashboard.putNumber("rv", rv);
-		if (Math.sqrt(vx * vx + vy * vy + rv * rv) > 0.3) {
-			double r = Math.sqrt(robotWidth * robotWidth + robotHeight * robotHeight) / 2;
-			double[] driveSpeeds = new double[4];
-			double maxDriveSpeed = 0;
-			for (int i = 0; i < 4; i++) {
-				double wheelAngle = Math.atan2(robotWidth, robotHeight);
-				if (i == 0 || i == 3) {
-					wheelAngle *= -1;
-				}
-				if (i == 2 || i == 3) {
-					wheelAngle += Math.PI;
-				}
-				wheelAngle += currentAngle;
-				double dx = r * Math.cos(wheelAngle);
-				double dy = r * Math.sin(wheelAngle);
-				double actualvx = vx + rv * dy;
-				double actualvy = vy - rv * dx;
-				double wheelTheta = Math.atan2(actualvy, actualvx);
-				double speed = Math.sqrt(actualvx * actualvx + actualvy * actualvy);
-				driveSpeeds[i] = speed;
-				maxDriveSpeed = Math.max(maxDriveSpeed, Math.abs(speed));
-				wheels[i].setTargetPosition(wheelTheta - currentAngle);
-				SmartDashboard.putNumber("target position " + i, wheelTheta - currentAngle);
+		double r = Math.sqrt(robotWidth * robotWidth + robotHeight * robotHeight) / 2;
+		double maxDriveSpeed = 0;
+		for (int i = 0; i < 4; i++) {
+			double wheelAngle = Math.atan2(robotWidth, robotHeight);
+			if (i == 0 || i == 3) {
+				wheelAngle *= -1;
 			}
-			double driveRatio = Math.min(1, 1 / maxDriveSpeed) / 2; // should only scale down if the wheels should go really fast
-			for (int i = 0; i < 4; i++) {
-				wheels[i].setDriveSpeed(driveSpeeds[i] * driveRatio);
-				SmartDashboard.putNumber("drive speed " + i, driveSpeeds[i] * driveRatio);
+			if (i == 2 || i == 3) {
+				wheelAngle += Math.PI;
 			}
-		} else {
-			for (Wheel wheel : wheels) {
-				wheel.setDriveSpeed(0);
+			wheelAngle += currentAngle;
+			double dx = r * Math.cos(wheelAngle);
+			double dy = r * Math.sin(wheelAngle);
+			double actualvx = vx + rv * dy;
+			double actualvy = vy - rv * dx;
+			double wheelTheta = Math.atan2(actualvy, actualvx);
+			double speed = Math.sqrt(actualvx * actualvx + actualvy * actualvy);
+			maxDriveSpeed = Math.max(maxDriveSpeed, Math.abs(speed));
+			double targetPosition = wheelTheta - currentAngle;
+			if (speed > 0.1) {
+				wheels[i].setDriveSpeed(speed);
+				SmartDashboard.putNumber("speed " + i, speed);
+				wheels[i].setTargetPosition(targetPosition);
+				SmartDashboard.putNumber("target position " + i, targetPosition);
+			} else {
+				wheels[i].setDriveSpeed(0);
+				SmartDashboard.putNumber("speed " + i, 0);
 			}
+			SmartDashboard.putNumber("gyro", currentAngle);
 		}
-		System.out.println(currentAngle);
 	}
 
 	@Override
