77039d4ef879601fd3cce527cded64408b3851a3
sjiang647 <sjiang647@gmail.com>
2018-01-23 10:06:03 -0800

    add intake mech

---
+++ src/org/usfirst/frc/team192/mechs/Intake.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team192.mechs;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Intake{
+
+	private TalonSRX left, right, vertical;
+	private int up_position = 0;
+	private int down_position = 0;
+	private int flywheel_speed = 0;
+	
+	public Intake(TalonSRX leftMotor, TalonSRX rightMotor, TalonSRX verticalMotor) {
+		left = leftMotor;
+		right = rightMotor;
+		vertical = verticalMotor;
+	}
+	
+	public void rollUp() {
+		vertical.set(ControlMode.Position, up_position);
+	}
+	
+	public void rollDown() {
+		vertical.set(ControlMode.Position, down_position);
+	}
+	
+	public void intake() {
+		left.set(ControlMode.PercentOutput, flywheel_speed);
+		right.set(ControlMode.PercentOutput, flywheel_speed);
+	}
+	
+	public void output() {
+		left.set(ControlMode.PercentOutput, -flywheel_speed);
+		left.set(ControlMode.PercentOutput, -flywheel_speed);
+	}
+}
