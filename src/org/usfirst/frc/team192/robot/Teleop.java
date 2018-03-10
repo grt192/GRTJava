d171a8e73f087c306302c7ad40fb624505e1d94a
coolpeepz <colin.r.schultz@gmail.com>
2018-03-10 15:28:24 -0800

    Began fixing cancer

--- src/org/usfirst/frc/team192/robot/Teleop.java
+++ src/org/usfirst/frc/team192/robot/Teleop.java
@@ -1,20 +1,13 @@
 package org.usfirst.frc.team192.robot;
 
-import java.awt.Point;
-
-import org.usfirst.frc.team192.config.Config;
 import org.usfirst.frc.team192.mechs.Climber;
-import org.usfirst.frc.team192.mechs.Intake;
 import org.usfirst.frc.team192.mechs.Elevator;
+import org.usfirst.frc.team192.mechs.Intake;
 import org.usfirst.frc.team192.swerve.FullSwervePID;
-import org.usfirst.frc.team192.vision.ImageThread;
 import org.usfirst.frc.team192.vision.nn.RemoteVisionThread;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
@@ -24,63 +17,32 @@ public class Teleop {
 	private Climber climber;
 	private Intake intake;
 	private RemoteVisionThread vision;
-	private KalmanFilter filter;
-	private Point CAMCENTER = new Point(320, 240);
-	private VisionPID pid;
 
 	DigitalInput innerLimitSwitch;
 	DigitalInput outerLimitSwitch;
-	//Encoder eencoder;
 
 	private FullSwervePID swerve;
-//  private boolean zeroing;
-//	private int index;
-	
-	//private Point CAMCENTER;
-  
-	public enum RobotState{
-		NothingState,
-		StartPickupState,
-		MovingToBlock,
-		PickingUpBlock,
-		ClampBlock,
-		EndPickup
-	}
-	
-	private RobotState destination;
-	private RobotState pickupState = RobotState.NothingState;
 
 	public Teleop(FullSwervePID swerve, Intake intake, Elevator elevator, Gyro gyro) {
 		xbox = new XboxController(1);
-		//climber = new Climber());
+		// climber = new Climber());
 		// 1 is the gear, 0 is main, 2 is the right
 		this.swerve = swerve;
-		this.vision = vision;
-		//CAMCENTER.x = 320;
-		//CAMCENTER.y = 240;
+		// CAMCENTER.x = 320;
+		// CAMCENTER.y = 240;
 		this.intake = intake;
 		this.elevator = elevator;
-		filter = new KalmanFilter();
-		pid = new VisionPID(vision, swerve, gyro);
 		init();
 
-//		this.zeroing = false;
+		// this.zeroing = false;
 	}
 
 	public void init() {
 		// 0 and 1 are digital input ports
-		//eencoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X); 
+		// eencoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
 	}
 
 	public void periodic() {
-		//climb code
-//		if (xbox.getXButtonPressed()) {
-//			climber.climb();
-//		}
-//		if (xbox.getXButtonReleased()) {
-//			climber.stopClimb();
-//		}
-		//pickup code
 		if (xbox.getXButtonPressed()) {
 			intake.moveCenterPickup(xbox);
 		}
@@ -90,74 +52,15 @@ public class Teleop {
 		if (xbox.getBumperPressed(Hand.kLeft)) {
 			elevator.breakElevator();
 		}
-		
-		//right trigger
+
+		// right trigger
 		intake.moveWheels(xbox);
-		
-		//elevator code
-		//right xbox axis
+
+		// elevator code
+		// right xbox axis
 		elevator.manualControl(xbox);
 		if (xbox.getY() < -.1) {
 			intake.movePickupOut();
 		}
-		
-//		//vision code
-//		if (xbox.getAButtonPressed()) {
-//			pickupState = RobotState.StartPickupState;
-//
-//			switch (pickupState) {
-//				case NothingState:
-//					break;
-//				case StartPickupState:
-//					elevator.moveToGroundPosition();
-//					if (vision.hasTarget()) {
-//						pickupState = RobotState.MovingToBlock;
-//					}
-//				case MovingToBlock:
-//					double errorx = filter.equations(vision.getCenter(), swerve.returnrv());
-//					swerve.setWithAngularVelocity(0, 0, errorx);
-//					swerve.updateAutonomous();
-//					if (Math.abs(errorx) < 10) {
-//						pickupState = RobotState.PickingUpBlock;
-//					}
-//				case PickingUpBlock:
-//					double errory = vision.getCenter().y - CAMCENTER.y;
-//					intake.autonPickup();
-//					swerve.setWithAngularVelocity(0, errory, 0);
-//					if (Math.abs(errory) < 10) {
-//						pickupState = RobotState.ClampBlock;
-//					}
-//				case ClampBlock:
-//					double errory2 = vision.getCenter().y - CAMCENTER.y;
-//					intake.autonClamp();
-//					if (errory2 < -10) {
-//						pickupState = RobotState.EndPickup;
-//					}
-//				case EndPickup:
-//					System.out.println("block picked up");
-//				}
-//		}
-	
-//	if (!zeroing && xbox.getAButton() && xbox.getXButton()) {
-//	zeroing = true;
-//	index = 0;
-//	System.out.println("zeroing");
-//	return;
-//}
-//
-//if (zeroing && xbox.getBButtonPressed()) {
-//	index++;
-//	System.out.println("changing wheels");
-//	if (index == 4) {
-//		zeroing = false;
-//		swerve.zero();
-//		return;
-//	}
-//}
-//
-//if (zeroing) {
-//	swerve.zeroWithInputs(index, xbox);
-//	return;
-//}
 	}
 }
