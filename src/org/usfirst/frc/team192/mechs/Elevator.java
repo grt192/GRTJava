63e166c0be8d918fec8d640c1ba1124c28ac4faa
Jack Jefferson <eptgen@gmail.com>
2018-03-29 10:46:20 -0700

    unused imports

--- src/org/usfirst/frc/team192/mechs/Elevator.java
+++ src/org/usfirst/frc/team192/mechs/Elevator.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.hal.PowerJNI;
 
 public class Elevator {
 
